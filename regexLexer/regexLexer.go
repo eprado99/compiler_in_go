1a0678bd710af42cc9383769403603e0c4aa95da
eprado99 <a01570318@itesm.mx>
2023-04-28 11:35:07 -0600

    refactored regexLexer.go to make use of json as config to set keywords and patterns instead of using flags

--- regexLexer/regexLexer.go
+++ regexLexer/regexLexer.go
@@ -1,10 +1,101 @@
 package regexLexer
 
 import (
-	"fmt"
+	"encoding/json"
 	"regexp"
-	"strings"
 )
+
+type Lexer struct {
+	patternsArray []TokenType
+	keywordsArray []string
+	patternsMap   map[TokenType]string
+}
+
+func (l *Lexer) Load() {
+	extraKeywords, extraPatterns, err := l.unmarshal()
+	if err == nil {
+		l.loadPatterns(extraPatterns)
+		l.loadKeywords(extraKeywords)
+	}
+}
+
+func (l *Lexer) loadPatterns(extraPatterns map[string]string) {
+	l.patternsMap = make(map[TokenType]string)
+	for k, v := range extraPatterns {
+		l.patternsMap[TokenType(k)] = v
+		l.patternsArray = append(l.patternsArray, TokenType(k))
+	}
+}
+
+func (l *Lexer) loadKeywords(extraKeywords []string) {
+	l.keywordsArray = append(l.keywordsArray, extraKeywords...)
+}
+
+func (l *Lexer) unmarshal() ([]string, map[string]string, error) {
+	type lexerJSON struct {
+		Keywords []string          `json:"keywords"`
+		Patterns map[string]string `json:"patterns"`
+	}
+	jsonStr := `
+	{
+		"keywords": [
+			"package",
+			"import",
+			"func",
+			"int",
+			"string",
+			"rune",
+			"bool",
+			"for",
+			"range",
+			"if",
+			"make",
+			"struct",
+			"type",
+			"self",
+			"return",
+			"var",
+			"byte",
+			"switch",
+			"case",
+			"break",
+			"continue"
+		],
+		"patterns": {
+			"whitespace": "\\s",
+			"number": "\\d",
+			"identifier": "[\\w]",
+			"string": "\\\"[^\"]*\\\"",
+			"comparisonOperators": "<|>|<=|>=",
+			"arithmeticOperators": "+|-|*|/",
+			"equal": "==",
+			"colon": "^:$",
+			"assignment": "^=$",
+			"addition": "\\+\\+",
+			"semicolon": ";",
+			"openCurly": "{",
+			"closeCurly": "}",
+			"openParenthesis": "\\(",
+			"closeParenthesis": "\\)",
+			"newLine": "\n",
+			"tab": "\t",
+			"residual": "%",
+			"comment": "\/\/.*",
+			"comma": ",",
+			"dot": "\\.",
+			"return": "\\r"
+		}
+			
+	}`
+
+	var lJ lexerJSON
+	err := json.Unmarshal([]byte(jsonStr), &lJ)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return lJ.Keywords, lJ.Patterns, err
+}
 
 type Token struct {
 	value     string
@@ -13,139 +104,12 @@ type Token struct {
 
 type TokenType string
 
-const (
-	Whitespace          TokenType = "whitespace"
-	Number              TokenType = "number"
-	Identifier          TokenType = "identifier"
-	Assignment          TokenType = "assignment"
-	Addition            TokenType = "addition"
-	Operators           TokenType = "operator"
-	ComparisonOperators TokenType = "comparison_operator"
-	Semicolon           TokenType = "semicolon"
-	OpenParenthesis     TokenType = "open_parenthesis"
-	CloseParenthesis    TokenType = "close_parenthesis"
-	OpenCurly           TokenType = "open_curly"
-	CloseCurly          TokenType = "close_curly"
-	Equal               TokenType = "equal"
-	Tab                 TokenType = "tab"
-	NewLine             TokenType = "new_line"
-	Return              TokenType = "return"
-	Keyword             TokenType = "keyword"
-	Residual            TokenType = "residual"
-	Comment             TokenType = "comment"
-	String              TokenType = "string"
-	Colon               TokenType = "colon"
-	Comma               TokenType = "comma"
-	Dot                 TokenType = "dot"
-	Reference           TokenType = "reference"
-)
+const Keyword TokenType = "keyword"
 
-var patternsArr = []TokenType{
-	Return,
-	Comment,
-	String,
-	Whitespace,
-	Number,
-	Identifier,
-	Equal,
-	Assignment,
-	Colon,
-	Addition,
-	Operators,
-	Semicolon,
-	OpenParenthesis,
-	CloseParenthesis,
-	OpenCurly,
-	CloseCurly,
-	Tab,
-	NewLine,
-	Residual,
-	Comma,
-	Reference,
-	Dot,
-}
-
-// "package",
-var keywordsArr = []string{
-	"package",
-	"import",
-	"func",
-	"int",
-	"string",
-	"rune",
-	"bool",
-	"for",
-	"range",
-	"if",
-	"make",
-	"struct",
-	"type",
-	"self",
-	"return",
-	"var",
-	"byte",
-	"switch",
-	"case",
-	"break",
-	"continue",
-}
-
-var patternsMap = map[TokenType]string{
-	Whitespace:          `\s`,
-	Number:              `\d`,
-	Identifier:          `[\w]`,
-	String:              `\"[^"]*\"`,
-	ComparisonOperators: `<|>|<=|>=`,
-	Operators:           `+|-|*|/`,
-	Equal:               `==`,
-	Colon:               `^:$`,
-	Assignment:          `^=$`,
-	Addition:            `\+\+`,
-	Semicolon:           `;`,
-	OpenCurly:           `{`,
-	CloseCurly:          `}`,
-	OpenParenthesis:     `\(`,
-	CloseParenthesis:    `\)`,
-	NewLine:             `\n`,
-	Tab:                 `\t`,
-	Residual:            `%`,
-	Comment:             `\/\/.*`,
-	Comma:               `,`,
-	Dot:                 `\.`,
-	Reference:           `&`,
-	Return:              `\r`,
-}
-
-func appendKeywords(extraKeywords []string) {
-	for _, v := range extraKeywords {
-		keywordsArr = append(keywordsArr, v)
-	}
-	fmt.Println(keywordsArr)
-}
-
-// InferredAssignment:  `:=`,
-func appendPatterns(extraPatterns []string) {
-	for _, v := range extraPatterns {
-		extraPatternArr := strings.Split(v, "0")
-		var newToken TokenType = TokenType(extraPatternArr[0])
-		newPattern := extraPatternArr[1]
-		patternsArr = append(patternsArr, newToken)
-		patternsMap[newToken] = newPattern
-	}
-	fmt.Println(patternsArr)
-	fmt.Println(patternsMap)
-}
-
-func processFlag(flag string) []string {
-	trimmedFlag := strings.TrimSpace(flag)
-	flagArray := strings.Split(trimmedFlag, ",")
-	return flagArray
-}
-
-func GetToken(lexeme string, tokenArr *[]Token) {
-	for _, pattern := range patternsArr {
+func GetToken(l Lexer, lexeme string, tokenArr *[]Token) {
+	for _, pattern := range l.patternsArray {
 		matchKeyword := false
-		for _, keyword := range keywordsArr {
+		for _, keyword := range l.keywordsArray {
 			if keyword == lexeme {
 				*tokenArr = append(*tokenArr, Token{value: lexeme, tokenType: Keyword})
 				matchKeyword = true
@@ -155,30 +119,23 @@ func GetToken(lexeme string, tokenArr *[]Token) {
 		if matchKeyword {
 			break
 		}
-		matched, _ := regexp.MatchString(patternsMap[pattern], lexeme)
+		matched, _ := regexp.MatchString(l.patternsMap[pattern], lexeme)
 		if matched {
 			*tokenArr = append(*tokenArr, Token{value: lexeme, tokenType: pattern})
 			break
 		}
 	}
 }
-func GetTokens(code string, keywordsFlag string, patternFlag string) []Token {
-	var tokenArr []Token
 
+func GetTokens(code string, keywordsFlag string, patternFlag string) []Token {
+	l := Lexer{}
+	var tokenArr []Token
+	l.Load()
 	pattern := regexp.MustCompile(`\r|\/\/.*| |[A-z0-9]+|%|==|:=|^=$|\+\+|\+|\(|\)|<|>|>=|<=|;|{|}|\"[^"]*\"|:|,|\.|\*|&`)
 	lexemes := pattern.FindAllString(code, -1)
-	if keywordsFlag != "" {
-		extraKeywordsArr := processFlag(keywordsFlag)
-		appendKeywords(extraKeywordsArr)
-	}
-
-	if patternFlag != "" {
-		extraPatternArray := processFlag(patternFlag)
-		appendPatterns(extraPatternArray)
-	}
 
 	for _, lexeme := range lexemes {
-		GetToken(lexeme, &tokenArr)
+		GetToken(l, lexeme, &tokenArr)
 	}
 	return tokenArr
 }
